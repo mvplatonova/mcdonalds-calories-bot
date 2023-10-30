82ad54c7bfb65040f03fbde15c11d71b02500294
mariaplatonova <maria.platonova@softcom.su>
2023-10-30 23:10:37 +0300

    created bot project

---
+++ src/main/java/com/example/bot2/Constants.java
@@ -0,0 +1,28 @@
+package com.example.bot2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.springframework.core.io.ClassPathResource;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Constants {
+    public static final String BOT_NAME = "BestMcDonaldsCaloriesBot";
+    public static final String TOKEN;
+
+    static {
+        TOKEN = readToken();
+    }
+
+    private static String readToken() {
+        try {
+            File resource = new ClassPathResource(".token").getFile();
+            return Files.readString(resource.toPath());
+        } catch (IOException e) {
+            log.error("Error while reading token", e);
+        }
+        return "";
+    }
+}
