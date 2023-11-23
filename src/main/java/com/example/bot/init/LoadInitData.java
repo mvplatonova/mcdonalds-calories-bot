03188f657af9c95874171bd68abeeac19ff682e2
mariaplatonova <maria.platonova@softcom.su>
2023-11-23 14:17:29 +0300

    Added init data

---
+++ src/main/java/com/example/bot/init/LoadInitData.java
@@ -0,0 +1,20 @@
+package com.example.bot.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LoadInitData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+  
+        
+    }
+    
+}
